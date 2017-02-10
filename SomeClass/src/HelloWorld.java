3233335f9ef0e481ddeab0e1ddcecaf9b6b44a5c
Strekalovskyi D <ortin@i.ua>
2017-02-10 16:28:41 +0200

    dsfsdfds
    (cherry picked from commit 87a73d1f40b67eeb79089154a8846623df1e99a5)

--- SomeClass/src/HelloWorld.java
+++ SomeClass/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         People people = new People();
-        people.name("sdas");
+        people.name("sda");
         people.age(51);
         System.out.println();
     }
